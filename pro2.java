4cc42b60d591b05c51cad3a6816e5232a7889c9a
jyothi <jyoth2010@gmail.com>
2023-07-10 12:45:35 -0400

    comiit jyothi

---
+++ pro2.java
@@ -0,0 +1,13 @@
+public class pro2 {
+    public class sum {
+        public static void main(String[] args) {
+
+
+
+            int a=10;
+            int b=20;
+            int s=a+b;
+            System.out.println(s);
+
+        }}
+}
